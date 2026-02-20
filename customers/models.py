42c3a5d6bdc1d6591d749289e91a8e8ac88f10a6
lama suleiman <lamasuliman42@gmail.com>
2026-02-21 01:48:51 +0200

    Implemented XGBoost churn prediction model, training script, dataset, and ML service

--- customers/models.py
+++ customers/models.py
@@ -1,3 +1,32 @@
 from django.db import models
 
-# Create your models here.
+
+class Customer(models.Model):
+
+    customer_id = models.IntegerField(unique=True)
+
+    surname = models.CharField(max_length=100)
+
+    credit_score = models.IntegerField()
+
+    geography = models.CharField(max_length=50)
+
+    gender = models.CharField(max_length=10)
+
+    age = models.IntegerField()
+
+    tenure = models.IntegerField()
+
+    balance = models.FloatField()
+
+    num_of_products = models.IntegerField()
+
+    has_cr_card = models.IntegerField()
+
+    is_active_member = models.IntegerField()
+
+    churn_risk_score = models.FloatField(null=True, blank=True)
+
+
+    def __str__(self):
+        return f"{self.customer_id} - {self.surname}"
