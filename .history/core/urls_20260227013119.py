e763750698658cb8e4bad8af1352973603860b9e
afanyu <afanyuemma2002@gmail.com>
2026-02-27 01:49:58 +0200

    setup cronjobs

---
+++ .history/core/urls_20260227013119.py
@@ -0,0 +1,11 @@
+from django.urls import path
+
+from core import views
+
+urlpatterns = [
+    path("",          views.landing_page,  name="landing"),
+    path("login/",    views.login_page,    name="login_page"),
+    path("register/", views.register_page, name="register_page"),
+    path("logout/",   views.logout_page,   name="logout_page"),
+    path("ping/",     views.health_check,  name="health_check"),
+]
